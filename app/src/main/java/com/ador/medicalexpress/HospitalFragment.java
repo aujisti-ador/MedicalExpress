06ce4088b01c07eef4269032048db1e72993f0ef
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2017-05-06 02:32:51 +0600

    Done!

--- app/src/main/java/com/ador/medicalexpress/HospitalFragment.java
+++ app/src/main/java/com/ador/medicalexpress/HospitalFragment.java
@@ -3,6 +3,7 @@ package com.ador.medicalexpress;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,11 +19,17 @@ public class HospitalFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+      //  ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Hospitals Info");
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_hospital, container, false);
     }
 
--- app/src/main/java/com/ador/medicalexpress/MainActivity.java
+++ app/src/main/java/com/ador/medicalexpress/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -18,6 +22,13 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.ambulance,
             R.drawable.blood
     };
+
+    String[] tabsTitles = {
+            "Hospital",
+            "Nearby",
+            "Ambulance",
+            "blood"
+    } ;
     //Icons
 
 
@@ -27,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolBar);
+        toolbar.setTitle(tabsTitles[0]);
         setSupportActionBar(toolbar);
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         viewPager = (ViewPager) findViewById(R.id.viewPager);
@@ -37,9 +49,49 @@ public class MainActivity extends AppCompatActivity {
         viewPagerAdapter.addFragments(new BloodFragment(),"");
 
         viewPager.setAdapter(viewPagerAdapter);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                toolbar.setTitle(tabsTitles[position]);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
         tabLayout.setupWithViewPager(viewPager);
 
         setupTabIcons();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int res_id = item.getItemId();
+        if (res_id==R.id.action_about)
+        {
+            Toast.makeText(this, "About Us!", Toast.LENGTH_SHORT).show();
+        }
+        else if (res_id==R.id.action_share)
+        {
+            Toast.makeText(this, "Share!", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void setupTabIcons() {
