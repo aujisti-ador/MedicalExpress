bf4a53203810dd124f01997b1206081a59ca0b26
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2017-05-07 22:29:15 +0600

    Dr activity added

--- app/src/main/java/com/ador/medicalexpress/DoctorViewActivity.java
+++ app/src/main/java/com/ador/medicalexpress/DoctorViewActivity.java
@@ -2,12 +2,27 @@ package com.ador.medicalexpress;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class DoctorViewActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_view);
+        toolbar = (Toolbar) findViewById(R.id.toolBar);
+        toolbar.setTitle("Doctor Contact");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_dr,menu);
+
+        return true;
     }
 }
