172c8d2e8c7221891cd77857711ccfa018f71e1c
Fazle Rabbi Ador <aujisti.ador@gmail.com>
2017-05-07 22:48:18 +0600

    back home added!

--- app/src/main/java/com/ador/medicalexpress/DoctorViewActivity.java
+++ app/src/main/java/com/ador/medicalexpress/DoctorViewActivity.java
@@ -1,10 +1,12 @@
 package com.ador.medicalexpress;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class DoctorViewActivity extends AppCompatActivity {
     Toolbar toolbar;
@@ -16,6 +18,12 @@ public class DoctorViewActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolBar);
         toolbar.setTitle("Doctor Contact");
         setSupportActionBar(toolbar);
+
+        if (getSupportActionBar()!=null)
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
     }
 
     @Override
@@ -24,5 +32,12 @@ public class DoctorViewActivity extends AppCompatActivity {
         menuInflater.inflate(R.menu.menu_dr,menu);
 
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
     }
 }
